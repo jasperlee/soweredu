dbf4c7e48f1cb954c21fcd4668536fcc11fdfb8d
jasper <jasper@jasperdeMacBook-Pro.local>
2016-06-07 17:13:51 +0800

    修改配置文件

--- src/conf.php
+++ src/conf.php
@@ -1,9 +1,5 @@
 <?php
 class ADConf {
-	const	AccessId = "310902426204";
-	const	AccessKey = "4y4MWt8x7enylydwDkdh4m7Xg49turCM";
-	const 	TisId = "d8ee78aaba905764b326af6a90ba3653";
-	const 	DmsSKey = "s_ac043048798010e9cfdc42dbadec6f36";
 }
 $accessId = ADConf::AccessId;
 $accessKey = ADConf::AccessKey;
